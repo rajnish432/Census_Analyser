30edddea963dc2e3ccd4bf64231621d79c83c144
rajnish432 <rajnish.kahar1996@gmail.com>
2020-03-03 11:27:07 +0530

    ADDED: Loaded Indian State Census Data From CSV File

---
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,35 @@
+package censusanalyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class CensusAnalyser {
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            int numOfEntries = 0;
+            while (censusCSVIterator.hasNext())
+            {
+                IndiaCensusCSV indiaCensusCSV=censusCSVIterator.next();
+                numOfEntries++;
+            }
+            return numOfEntries;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+}
